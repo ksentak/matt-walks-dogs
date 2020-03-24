0992dd8865db49f4ac1ca40676cd9492a343629b
KSentak <k.sentak@outlook.com>
2020-03-24 13:28:02 -0400

    Uploaded to GitHub

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['message'];
+
+  $subject = "Message from website"
+
+  $mailTo = "k.sentak@outlook.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+
+  header("Location: index.php?mailsend")
+}
